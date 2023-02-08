30c445d4e78b152e38c972953d876fc8abaf8697
reactpd <cyb.8964803609@gmail.com>
2023-02-08 15:45:44 +0530

    update

--- src/routes/AdminRoutes/dashboard.js
+++ src/routes/AdminRoutes/dashboard.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const dashboard = express.Router()
+const leadsmodel = require("../../schema/models")
 
 
-dashboard.get('', (req, res) => {
-    res.send({data:"tapas"})
+dashboard.get('', async (req, res) => {
+    try {
+        const data = await leadsmodel.find().sort({_id:-1}).limit(10)
+        console.log(data)
+        res.send({ massage: "welcome to IG Server", data: data })
+
+    } catch (error) {
+        console.log(`dashboard-js error found ${error}`)
+    }
 })
 
 
